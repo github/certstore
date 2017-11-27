8c40d7de1ec0636e736733d71dda2c3992026283
Ben Toews <mastahyeti@gmail.com>
2017-11-27 09:42:50 -0700

    include individual security.framework headers

--- certstore_darwin.go
+++ certstore_darwin.go
@@ -4,7 +4,11 @@ package certstore
 #cgo CFLAGS: -x objective-c
 #cgo LDFLAGS: -framework CoreFoundation -framework Security
 #include <CoreFoundation/CoreFoundation.h>
-#include <Security/Security.h>
+#include <Security/SecKey.h>
+#include <Security/SecItem.h>
+#include <Security/SecImportExport.h>
+#include <Security/SecIdentity.h>
+#include <Security/SecCertificate.h>
 */
 import "C"
 import (

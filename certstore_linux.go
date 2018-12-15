721678d276a22d7c8b2831d26f9ae5d41fd35aa4
Mateusz Klatt <cherubim@satan.ie>
2018-12-15 22:28:25 +0000

    Initial commit

--- certstore_linux.go
+++ certstore_linux.go
@@ -1,14 +1,60 @@
 package certstore
 
-import "errors"
+/*
+#cgo CFLAGS: -I/usr/include/nss -I/usr/include/nspr
+#cgo LDFLAGS: -lnss3
+#include <nss.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+*/
+import "C"
+import (
+	"errors"
+	"fmt"
+	"syscall"
+	"unsafe"
+)
 
-// This will hopefully give a compiler error that will hint at the fact that
-// this package isn't designed to work on Linux.
-func init() {
-	CERTSTORE_DOESNT_WORK_ON_LINIX
+type Passwd struct {
+	PwName   string
+	PwPasswd string
+	PwUid    int
+	PwGid    int
+	PwGecos  string
+	PwDir    string
+	PwShell  string
 }
 
-// Implement this function, just to silence other compiler errors.
 func openStore() (Store, error) {
-	return nil, errors.New("certstore only works on macOS and Windows")
+	var passwd *Passwd = nil
+	passwdC, err := C.getpwuid(C.getuid())
+	if passwdC == nil {
+		if err == nil {
+			var e syscall.Errno
+			err = errors.New(e.Error())
+		}
+		return nil, errors.New(fmt.Sprintf("There was an error (%s) wen getting user info\n", error(err)))
+	} else {
+		passwd = &Passwd{
+			PwName:   C.GoString(passwdC.pw_name),
+			PwPasswd: C.GoString(passwdC.pw_passwd),
+			PwUid:    int(passwdC.pw_uid),
+			PwGid:    int(passwdC.pw_gid),
+			PwGecos:  C.GoString(passwdC.pw_gecos),
+			PwDir:    C.GoString(passwdC.pw_dir),
+			PwShell:  C.GoString(passwdC.pw_shell),
+		}
+		fmt.Printf("Home directory is: %s\n", passwd.PwDir)
+	}
+	if passwd == nil {
+		return nil, errors.New("Cannot locate nssdb store!\n")
+	}
+	name := fmt.Sprintf("sql:%s/.pki/nssdb/", passwd.PwDir)
+	nameC := C.CString(name)
+	defer C.free(unsafe.Pointer(nameC))
+	fmt.Printf("Opening: %s\n", name)
+	//C.NSS_InitReadWrite()
+	//C.NSS_Shutdown()
+	return nil, errors.New("Not using NSS yet!\n")
 }
